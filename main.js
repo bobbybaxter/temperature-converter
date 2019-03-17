4ef8d92a6009a96f32c4b39d048ba29a047dbe0e
Bob <bob.e.baxter@gmal.com>
2019-03-17 10:25:23 -0500

    convert button events work

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-console.log('hi');
+const tempInput = document.getElementById('tempInput');
+const radios = document.getElementsByClassName('radio');
+
+const printToDom = (divId, textToPrint) => {
+  const selectedDiv = document.getElementById(divId);
+  selectedDiv.innerHTML = textToPrint;
+};
+
+const newTempType = () => {
+  for (let i = 0; i < radios.length; i++) {
+    if (radios[i].checked){
+      console.log(radios[i].nextElementSibling.innerHTML)
+      // printToDom('tempOutput', radios[i].nextElementSibling.innerHTML);
+    }
+  }
+};
+
+newTempType();
+console.log(radios);
+
+const determineConverter = () => {
+ console.log(tempInput.value);
+ console.log(newTempType());
+ printToDom('tempOutput', tempInput.value);
+};
+
+convertBtn.addEventListener("click", determineConverter);
