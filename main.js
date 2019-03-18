e0f6dd276c25cb746e124ac5bebdca67a05a39ff
Bob <bob.e.baxter@gmal.com>
2019-03-17 20:56:00 -0500

    temperature conversion works

--- main.js
+++ main.js
@@ -1,27 +1,42 @@
 const tempInput = document.getElementById('tempInput');
-const radios = document.getElementsByClassName('radio');
+const convertBtn = document.getElementById('convertBtn');
+const radioC = document.getElementById('radioC');
+const radioF = document.getElementById('radioF');
+
+const toCelsius = () => {
+  unit = 'C';
+  finalTemp = (tempInput.value - 32) * .5556;
+  domStringBuilder(finalTemp, unit);
+};
+
+const toFahrenheit = () => {
+  unit = 'F';
+  finalTemp = (tempInput.value * 1.8) + 32;
+  domStringBuilder(finalTemp, unit);
+};
 
 const printToDom = (divId, textToPrint) => {
   const selectedDiv = document.getElementById(divId);
   selectedDiv.innerHTML = textToPrint;
 };
 
-const newTempType = () => {
-  for (let i = 0; i < radios.length; i++) {
-    if (radios[i].checked){
-      console.log(radios[i].nextElementSibling.innerHTML)
-      // printToDom('tempOutput', radios[i].nextElementSibling.innerHTML);
-    }
+const domStringBuilder = (finalTemp, unit) => {
+  let domString = [];
+  domString += `<h2>${finalTemp} degrees ${unit}</h2>`;
+
+  printToDom('tempOutput', domString);
+}
+
+const determineConverter = () => {
+  if (radioC.checked) {
+    toFahrenheit();
+  } else if (radioF.checked) {
+    toCelsius();
   }
 };
 
-newTempType();
-console.log(radios);
-
-const determineConverter = () => {
- console.log(tempInput.value);
- console.log(newTempType());
- printToDom('tempOutput', tempInput.value);
+const init = () => {
+  convertBtn.addEventListener('click', determineConverter);
 };
 
-convertBtn.addEventListener("click", determineConverter);
+init();
